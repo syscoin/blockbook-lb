64112f20e151ff0688c1d163d26e537eaea4b792
matheushent <matheustosta66@gmail.com>
2021-04-12 17:06:08 -0300

    bump all commands from subprocess to os

--- src/blockbook_ops.py
+++ src/blockbook_ops.py
@@ -39,7 +39,9 @@ class BlockbookOps:
 
     def install_syscoin_testnet_backend(self):
 
-        subprocess.call(["sh", os.path.join("scripts", "install-backend.sh")])
+        # subprocess.call(["sh", os.path.join("scripts", "install-backend.sh")])
+        os.system("cd blockbook/build && \
+                    apt install -y ./backend-syscoin-testnet_4.2.0.14-satoshilabs-1_amd64.deb")
 
     def start_syscoin_testnet_backend(self):
 
@@ -49,7 +51,9 @@ class BlockbookOps:
 
     def build_syscoin_testnet(self):
 
-        subprocess.call(["sh", os.path.join("scripts", "build-app.sh")])
+        # subprocess.call(["sh", os.path.join("scripts", "build-app.sh")])
+        os.system("cd blockbook/build && \
+                    apt install -y ./blockbook-syscoin-testnet_0.3.54_amd64.deb")
 
     def start_syscoin_testnet(self):
 
