82a9a4f3e4d428b052b448e2acd682aa7a8ae40c
beniciowg <beniciowg@gmail.com>
2021-12-01 09:16:12 -0300

    update: commit hash

--- scripts/clone-blockbook.sh
+++ scripts/clone-blockbook.sh
@@ -12,4 +12,4 @@ pip3 install watchgod
 git clone https://github.com/syscoin/blockbook.git
 
 cd blockbook && \
-    git reset b09c770cc6a0dba95f4db2e48f61037f95bc6085 --hard
+    git reset ca9bb8170dc11f1d56678e5789cd17c98b919214 --hard
